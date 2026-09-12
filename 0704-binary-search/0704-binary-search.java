e94a3f7bde95b14a5307044b2cda8b1b97a3907c
Lakshya Yadav <lakshya2137@gmail.com>
2026-09-12 09:24:01 +0530

    Time: 0 ms (100%), Space: 47.9 MB (94.31%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int search(int[] arr, int target) {
+        int s = 0;
+        int e = arr.length - 1;
+
+        while(s <= e){
+            int mid = s + (e - s) / 2 ;
+            if(target == arr[mid]){
+                return mid;
+            }
+            if(target > arr[mid]){
+                s = mid + 1;
+            }else{
+                e = mid - 1;
+            }
+        }
+        return - 1 ;
+    }
+}
