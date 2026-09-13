223fff008304779a75b7aecc7e7ad1bdc8ad15ad
Lakshya Yadav <lakshya2137@gmail.com>
2026-09-13 12:45:27 +0530

    Time: 1 ms (25.95%), Space: 43 MB (51.66%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.java
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -1,5 +1,15 @@
 class Solution {
     public String toLowerCase(String s) {
-        return s.toLowerCase();
+        StringBuilder ans = new StringBuilder();
+
+        for(char ch : s.toCharArray()) {
+            if(ch >= 'A' && ch <= 'Z') {
+                ch = (char)(ch + 32);
+            }
+
+            ans.append(ch);
+        }
+
+        return ans.toString();
     }
 }
